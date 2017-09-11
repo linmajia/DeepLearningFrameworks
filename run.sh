9f86c7813957b042154eb7c0f70b4b16f89f8ed0
HX Lin <linmajia@live.com>
2017-09-11 22:00:41 +0800

    Add run.sh script for selected frameworks.

---
+++ run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+n=0
+array=(CNTK Tensorflow_NHWC Keras_CNTK Keras_TF MXNet Gluon Chainer)
+for i in "${array[@]}"
+do
+	echo Test $n: $i
+	echo ""
+	rm -f ${i}_CIFAR.log
+	ipython ${i}_CIFAR.py 2>&1 | tee ${i}_CIFAR.log
+	let n=n+1
+	echo -e "\n\n\n"
+done
