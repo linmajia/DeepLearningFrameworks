ca312f3c416dbe8a9df7b1995f8ba4904343e160
HX Lin <linmajia@live.com>
2017-09-12 15:41:34 +0800

    Convert notebook to python if necessary.

--- run.sh
+++ run.sh
@@ -7,6 +7,10 @@ do
 	echo Test $n: $i
 	echo ""
 	rm -f ${i}_CIFAR.log
+	if ! [ -f ${i}_CIFAR.py ]
+		then
+			ipython nbconvert --to python ${i}_CIFAR.ipynb
+	fi
 	ipython ${i}_CIFAR.py 2>&1 | tee ${i}_CIFAR.log
 	let n=n+1
 	echo -e "\n\n\n"
