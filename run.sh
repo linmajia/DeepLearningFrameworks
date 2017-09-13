b5e2ae18ff3e90b97efb89e8094a70371a254c2e
HX Lin <linmajia@live.com>
2017-09-13 14:05:07 +0800

    Use current time to create result directory.

--- run.sh
+++ run.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-out_dir="results/$1_$2"
+out_dir="results/$(date +%Y_%m_%d_%H_%M)"
 if [ ! -d ${out_dir} ]
 	then
 		mkdir -p ${out_dir}
 fi
 rm -fr ${out_dir}/*
-
-if [ "$1" == "gpu" ]
-	then
-		c=0
-		visible_devices=""
-		while [ $c -lt $2 ]; do
-			visible_devices="${visible_devices} ${c}"
-			let c=c+1
-		done
-		CUDA_VISIBLE_DEVICES="${visible_devices}"
-		export CUDA_VISIBLE_DEVICES
-fi
 
 n=0
 array=(CNTK Tensorflow_NHWC Keras_CNTK Keras_TF MXNet Gluon Chainer)
