21073c0c7b7cbd1ea707064651dcbd8a3bc3bc00
HX Lin <linmajia@live.com>
2017-09-13 12:49:41 +0800

    Add running mode and devices support.

--- run.sh
+++ run.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
+out_dir="results/$1_$2"
+if [ ! -d ${out_dir} ]
+	then
+		mkdir -p ${out_dir}
+fi
+rm -fr ${out_dir}/*
+
+if [ "$1" == "gpu" ]
+	then
+		c=0
+		visible_devices=""
+		while [ $c -lt $2 ]; do
+			visible_devices="${visible_devices} ${c}"
+			let c=c+1
+		done
+		CUDA_VISIBLE_DEVICES="${visible_devices}"
+		export CUDA_VISIBLE_DEVICES
+fi
+
 n=0
 array=(CNTK Tensorflow_NHWC Keras_CNTK Keras_TF MXNet Gluon Chainer)
 for i in "${array[@]}"
 do
 	echo Test $n: $i
 	echo ""
-	rm -f ${i}_CIFAR.log
 	if ! [ -f ${i}_CIFAR.py ]
 		then
 			ipython nbconvert --to python ${i}_CIFAR.ipynb
 	fi
-	ipython ${i}_CIFAR.py 2>&1 | tee ${i}_CIFAR.log
+	log_file="${out_dir}/${i}_CIFAR.log"
+	rm -f ${log_file}
+	ipython ${i}_CIFAR.py 2>&1 | tee ${log_file}
 	let n=n+1
 	echo -e "\n\n\n"
 done
