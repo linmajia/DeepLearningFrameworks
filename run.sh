054494e0d5bbd3991696b1cb7ba3fcd0a020ce83
HX Lin <linmajia@live.com>
2017-09-17 20:57:16 +0800

    1. For macOS don't run CNTK tests. 2. Disable ipython nbconvert outputs.

--- run.sh
+++ run.sh
@@ -8,13 +8,24 @@ fi
 rm -fr ${out_dir}/*
 
 n=0
-array=(CNTK Tensorflow_NHWC Keras_CNTK Keras_TF MXNet Gluon Chainer)
-for i in "${array[@]}"
+
+case "$(uname -s)" in
+
+   Darwin)
+     test_array=(Tensorflow_NHWC Keras_TF MXNet Gluon Chainer)
+     ;;
+
+   *)
+     test_array=(CNTK Tensorflow_NHWC Keras_CNTK Keras_TF MXNet Gluon Chainer)
+     ;;
+esac
+
+for i in "${test_array[@]}"
 do
 	echo Test $n: $i
 	echo ""
 	rm -f ${i}_CIFAR.py
-	ipython nbconvert --to python ${i}_CIFAR.ipynb
+	ipython nbconvert --to python ${i}_CIFAR.ipynb >/dev/null 2>&1
 	log_file="${out_dir}/${i}_CIFAR.log"
 	rm -f ${log_file}
 	ipython ${i}_CIFAR.py 2>&1 | tee ${log_file}
